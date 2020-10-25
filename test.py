82e2ce263ce37e9e025d6ec34af3575dea19f758
Sophia Zhou <slzhou73@gmail.com>
2020-10-25 16:05:42 -0400

    these files are for bot based on number rather than cursor placement

---
+++ test.py
@@ -0,0 +1,8 @@
+from py_imessage import imessage
+import time as time
+
+phone = "7745340078"
+
+for i in range(7):
+    guid = imessage.send(phone, "oop deviated septum check oop")
+    time.sleep(3)
