d7517486d13cb01a5d857a604a1cf2bcba43e1ee
Sophia Zhou <slzhou73@gmail.com>
2020-10-26 23:20:01 -0400

    decided to keep the spammer manual, didn't use time

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import time as time
 
 phone = "7745340078"
 
-for i in range(7):
-    guid = imessage.send(phone, "oop deviated septum check oop")
-    time.sleep(3)
+guid = imessage.send(phone, "oop deviated septum check oop")
+guid = imessage.send(phone, "overthinking? hahaha, no <3")
+guid = imessage.send(phone, "did you sleep at least 8 hours last night? i didnt think so")
+guid = imessage.send(phone, "exercise is the key to a long and healthy life")
+guid = imessage.send(phone, "productivity cycles are the worst :/")
